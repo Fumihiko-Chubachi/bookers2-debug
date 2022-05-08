c80b0ca9e387f5ec36de4972d0c0b798b3e1368e
EC2 Default User <aria.bravemen@gmail.com>
2022-05-08 02:19:27 +0000

    up

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,7 +9,7 @@ class RelationshipsController < ApplicationController
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-		redirect_to request.referer
+		#redirect_to request.referer
   end
 
   def followings
